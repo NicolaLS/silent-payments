f2079c2c3ee46a5f849e3ab09e2b2ae737d9f278
Nicola Leonardo Susca <nicolaleonardo.susca@gmail.com>
2025-03-21 14:51:13 -0600

    database: Create SqlitePool wrapper
    Create module for DB stuff and a struct `Store` that wraps the
    `SqlitePool` for a better API.
    - might want to implement a trait for DB interface and implement it for
      SqlitePool later for mocking.
    - Absolutely have to refactor this, somethings wrong, I don't know what
      though.

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,5 +1,5 @@
 use bitcoincore_rpc::{Auth, Client};
-use std::str::FromStr;
+use store::Store;
 use sync::Syncer;
 use tokio::sync::mpsc;
 
@@ -8,10 +8,10 @@ use axum::{
     extract::{Path, State},
     routing::get,
 };
-use sqlx::{SqlitePool, sqlite::SqliteConnectOptions};
 
 mod rpc;
 mod silentpayments;
+mod store;
 mod sync;
 
 // TODO: Bitcoin Core RPC Config.
@@ -22,15 +22,14 @@ pub struct ServerConfig {
 
 pub struct Server {
     cfg: ServerConfig,
-    db: SqlitePool,
+    db: Store,
 }
 
 impl Server {
     pub async fn new(cfg: ServerConfig) -> Result<Self, Box<dyn std::error::Error>> {
-        let options = SqliteConnectOptions::from_str(&cfg.db_url)?.create_if_missing(true);
-        let pool = SqlitePool::connect_with(options).await?;
+        let db = Store::new(cfg.db_url.clone()).await?;
 
-        Ok(Self { cfg, db: pool })
+        Ok(Self { cfg, db })
     }
 
     pub async fn run(&self) -> Result<(), Box<dyn std::error::Error>> {
@@ -41,15 +40,16 @@ impl Server {
 
         // Run syncer.
         let mut syncer = Syncer::new(client, 1000);
-        let sync_from_height = get_synced_blocks_height(&self.db).await as u64;
+        //let sync_from_height = get_synced_blocks_height(&self.db).await as u64;
+        let sync_from_height = self.db.get_synced_blocks_height().await as u64;
         tokio::task::spawn(async move { syncer.sync_from(sync_from_height, sync_tx).await });
 
         // Receive blocks from syncer and add them to DB.
-        let sync_pool = self.db.clone();
+        let sync_db = self.db.clone();
         tokio::task::spawn(async move {
             while let Some(msg) = sync_rx.recv().await {
                 println!("new block: {:?}", msg);
-                add_block(msg, &sync_pool).await;
+                sync_db.add_block(msg).await;
             }
         });
 
@@ -58,6 +58,7 @@ impl Server {
 
         let app = Router::new()
             .route("/", get(root))
+            .route("/blocks/tip", get(get_chain_tip))
             .route("/blocks/{height}", get(get_block_by_height))
             .with_state(state);
 
@@ -74,117 +75,10 @@ pub async fn root() -> &'static str {
     "Silent Payment Server"
 }
 
-pub async fn get_block_by_height(
-    State(pool): State<SqlitePool>,
-    Path(height): Path<i64>,
-) -> String {
+pub async fn get_chain_tip(State(db): State<Store>) -> String {
+    // TODO: Figure out how to return a number lol.
+    db.get_synced_blocks_height().await.to_string()
+}
+pub async fn get_block_by_height(State(_db): State<Store>, Path(_height): Path<i64>) -> String {
     todo!()
-}
-
-// DB
-
-struct BlockModel {
-    // Primary key
-    height: i64,
-    hash: String,
-    tx_count: i64,
-}
-
-struct TransactionModel {
-    // Primary key
-    id: Option<i64>,
-    // References block(height)
-    block: i64,
-    txid: String,
-    scalar: String,
-    // Hex encoded public scalar
-}
-
-struct OutputModel {
-    // Primary key
-    id: Option<i64>,
-    // References transaction(id).
-    tx: i64,
-    vout: i64,
-    value: i64,
-    // Hex encoded scriptPubKey
-    script_pub_key: String,
-}
-
-async fn add_block(block: silentpayments::SPBlock, pool: &SqlitePool) {
-    // FIXME: Sqlite uses i64 for everything but we use u64 a lot..
-    let block_model = BlockModel {
-        height: block.height as i64,
-        hash: block.hash,
-        tx_count: block.txs.len() as i64,
-    };
-
-    let mut db_tx = pool.begin().await.unwrap();
-
-    // Insert block meta.
-    sqlx::query!(
-        "INSERT INTO blocks (height, hash, tx_count) VALUES (?, ?, ?)",
-        block_model.height,
-        block_model.hash,
-        block_model.tx_count
-    )
-    .execute(&mut *db_tx)
-    .await
-    .unwrap();
-
-    // FIXME: Batch insert all txs and outputs would be better. But I struggled to find out how to
-    // do this as I need the tx primary keys when inserting the outputs...there is probably a way
-    // but for now let's just add every tx and output iteratively...
-
-    // Insert transactions and its outputs.
-    for block_tx in block.txs.iter() {
-        let tx_model = TransactionModel {
-            id: None,
-            block: block.height as i64,
-            txid: block_tx.tx.compute_txid().to_string(),
-            scalar: block_tx.scalar.clone(),
-        };
-
-        let query_result = sqlx::query!(
-            r#"
-        INSERT INTO transactions (id, block, txid, scalar) VALUES (NULL, ?, ?, ?)
-        "#,
-            tx_model.block,
-            tx_model.txid,
-            tx_model.scalar
-        )
-        .execute(&mut *db_tx)
-        .await
-        .unwrap();
-
-        let tx_id = query_result.last_insert_rowid();
-
-        for (i, output) in block_tx.tx.output.iter().enumerate() {
-            let value_sat = output.value.to_sat() as i64;
-            let script_pubkey_hex = output.script_pubkey.to_hex_string();
-            let vout = i as i64;
-            sqlx::query!(
-                r#"
-        INSERT INTO outputs (id, tx, vout, value, script_pub_key) VALUES (NULL, ?, ?, ?, ?)
-        "#,
-                tx_id,
-                vout,
-                value_sat,
-                script_pubkey_hex,
-            )
-            .execute(&mut *db_tx)
-            .await
-            .unwrap();
-        }
-    }
-
-    db_tx.commit().await.unwrap();
-}
-
-async fn get_synced_blocks_height(pool: &SqlitePool) -> i64 {
-    sqlx::query_scalar!("SELECT MAX(height) FROM blocks")
-        .fetch_one(pool)
-        .await
-        .unwrap()
-        .unwrap()
 }
---
+++ server/src/store.rs
@@ -0,0 +1,159 @@
+// TODO: Consider implementing a trait "DB" for better testability.
+
+use std::str::FromStr;
+
+use sqlx::{Sqlite, SqlitePool, Transaction, sqlite::SqliteConnectOptions};
+
+use crate::silentpayments;
+
+pub struct BlockModel {
+    pub height: i64,
+    pub hash: String,
+    pub tx_count: i64,
+}
+
+pub struct TransactionModel {
+    #[allow(dead_code)]
+    pub id: Option<i64>,
+    pub block: i64,
+    pub txid: String,
+    pub scalar: String,
+}
+
+pub struct OutputModel {
+    #[allow(dead_code)]
+    pub id: Option<i64>,
+    pub tx: i64,
+    pub vout: i64,
+    pub value: i64,
+    pub script_pub_key: String,
+}
+
+#[derive(Clone)]
+pub struct Store {
+    pool: SqlitePool,
+}
+
+impl Store {
+    pub async fn new(url: String) -> Result<Self, Box<dyn std::error::Error>> {
+        let options = SqliteConnectOptions::from_str(&url)?.create_if_missing(true);
+        let pool = SqlitePool::connect_with(options).await?;
+        Ok(Self { pool })
+    }
+
+    pub async fn get_synced_blocks_height(&self) -> i64 {
+        sqlx::query_scalar!("SELECT MAX(height) FROM blocks")
+            .fetch_one(&self.pool)
+            .await
+            .unwrap()
+            .unwrap()
+    }
+
+    async fn add_output(db_tx: &mut Transaction<'static, Sqlite>, output_model: OutputModel) {
+        sqlx::query!(
+            r#"
+        INSERT INTO outputs (id, tx, vout, value, script_pub_key) VALUES (NULL, ?, ?, ?, ?)
+        "#,
+            output_model.tx,
+            output_model.vout,
+            output_model.value,
+            output_model.script_pub_key,
+        )
+        .execute(&mut **db_tx)
+        .await
+        .unwrap();
+    }
+
+    async fn add_transaction(
+        db_tx: &mut Transaction<'static, Sqlite>,
+        tx: TransactionModel,
+    ) -> i64 {
+        let query_result = sqlx::query!(
+            r#"
+        INSERT INTO transactions (id, block, txid, scalar) VALUES (NULL, ?, ?, ?)
+        "#,
+            tx.block,
+            tx.txid,
+            tx.scalar,
+        )
+        .execute(&mut **db_tx)
+        .await
+        .unwrap();
+
+        query_result.last_insert_rowid()
+    }
+    async fn add_block_meta(db_tx: &mut Transaction<'static, Sqlite>, block_model: BlockModel) {
+        sqlx::query!(
+            "INSERT INTO blocks (height, hash, tx_count) VALUES (?, ?, ?)",
+            block_model.height,
+            block_model.hash,
+            block_model.tx_count
+        )
+        .execute(&mut **db_tx)
+        .await
+        .unwrap();
+    }
+    pub async fn add_block(&self, block: silentpayments::SPBlock) {
+        let mut db_tx = self.pool.begin().await.unwrap();
+
+        let block_model_height = block.height.try_into().expect("FIXME: Store as BLOB");
+        let block_model_tx_count = block
+            .txs
+            .len()
+            .try_into()
+            .expect("txs length not larger than i64");
+
+        let block_model = BlockModel {
+            height: block_model_height,
+            hash: block.hash,
+            tx_count: block_model_tx_count,
+        };
+
+        Store::add_block_meta(&mut db_tx, block_model).await;
+
+        // FIXME: Batch insert would be better, but now sure how to insert the outputs then since
+        // they refer to the transaction db primary keys.
+        for tx in block.txs.iter() {
+            let tx_model = TransactionModel {
+                id: None,
+                block: block.height as i64,
+                txid: tx.tx.compute_txid().to_string(),
+                scalar: tx.scalar.clone(),
+            };
+
+            let tx_id = Store::add_transaction(&mut db_tx, tx_model).await;
+
+            for (i, output) in tx.tx.output.iter().enumerate() {
+                let value = output.value.to_sat() as i64;
+                let script_pub_key = output.script_pubkey.to_hex_string();
+                let vout = i as i64;
+                let output_model = OutputModel {
+                    id: None,
+                    tx: tx_id,
+                    vout,
+                    value,
+                    script_pub_key,
+                };
+                Store::add_output(&mut db_tx, output_model).await;
+            }
+        }
+
+        db_tx.commit().await.unwrap();
+    }
+}
+
+impl From<silentpayments::SPBlock> for BlockModel {
+    fn from(value: silentpayments::SPBlock) -> Self {
+        let height = value.height.try_into().expect("FIXME: Store as BLOB");
+        let tx_count = value
+            .txs
+            .len()
+            .try_into()
+            .expect("txs length not larger than i64");
+        Self {
+            height,
+            hash: value.hash,
+            tx_count,
+        }
+    }
+}
